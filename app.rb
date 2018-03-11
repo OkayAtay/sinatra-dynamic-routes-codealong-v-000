8a22bd832ffe84825fcb834df9c8ff1fa34c30f8
Araba Taylor <araba.taylor20@gmail.com>
2018-03-11 19:40:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
-    @user_num1 = params[:num1]
-    @user_num2 = params[:num2]
+    @user_num1 = params[:num1].to_i
+    @user_num2 = params[:num2].to_i
     @product = @user_num1*@user_num2
     "#{@product}"
   end
