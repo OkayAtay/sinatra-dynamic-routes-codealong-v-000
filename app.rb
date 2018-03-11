6f887e8f2fc82eeedd03eba0325092ff7ad830a2
Araba Taylor <araba.taylor20@gmail.com>
2018-03-11 19:32:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,13 +16,13 @@ class App < Sinatra::Base
   # Code your final two routes here:
   get '/goodbye/:name' do
     @user_name = params[:name]
-    "Goodbye, #{user_name}."
+    "Goodbye, #{@user_name}."
   end
 
   get '/multiply/:num1/:num2' do
     @user_num1 = params[:num1]
     @user_num1 = params[:num2]
-    "#{num1}*#{num2}"
+    "#{@num1}*#{@num2}"
   end
-  
+
 end
